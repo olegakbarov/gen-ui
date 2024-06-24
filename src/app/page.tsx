a05385570d7707d1a6ff9cf4082224d83147dd4f
Venge M2 MBP <olegakbarov@gmail.com>
2024-06-23 20:29:28 -0700

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,165 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from "react";
+import ZodStream from "zod-stream";
 
-export default function Home() {
+import { jsonToZod } from "@/lib/json-to-zod";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { newSchema, newText } from "@/prompts";
+import { validIndexesFromSchema } from "@/lib/valid-indexes";
+
+const Main = () => {
+  const [prompt] = useState("go");
+
+  const [result, setResult] = useState({});
+  const [schema, setSchema] = useState(newSchema);
+  const [text, setText] = useState(newText);
+
+  const [loading, setLoading] = useState(false);
+
+  const startStream = async ({ url }: { url: string }) => {
+    setLoading(true);
+
+    try {
+      const completion = async () => {
+        const response = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify({
+            schema,
+            messages: [
+              {
+                content: text,
+                role: "user",
+              },
+            ],
+          }),
+        });
+
+        if (!response.ok || !response?.body)
+          throw new Error("failed to fetch completion");
+
+        return response?.body;
+      };
+      const zodSchema = jsonToZod(schema);
+
+      if (!zodSchema || zodSchema instanceof Error)
+        throw new Error("failed to parse schema");
+
+      const client = new ZodStream({});
+
+      const extractionStream = await client.create({
+        completionPromise: completion,
+        response_model: { schema: zodSchema, name: "Extractor" },
+      });
+
+      for await (const data of extractionStream) {
+        setResult(data);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    setLoading(false);
+  };
+
+  const submitMessage = async () => {
+    if (!prompt.length || loading) return;
+
+    try {
+      await startStream({
+        url: "/api/ai",
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const timeline = result?.timeline || [];
+  const validIndexes = validIndexesFromSchema(result);
+
+  console.log(validIndexes);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container">
+      <div className="flex items-start w-full">
+        <div className="w-full h-full flex-1 min-h-[400px]">
+          <h3 className="text-lg font-bold">Raw text</h3>
+          <Textarea
+            className="w-full h-full flex-1 min-h-[400px]"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className="w-full h-full flex-1 min-h-[400px]">
+          <h3 className="text-lg font-bold">Schema</h3>
+          <Textarea
+            className="w-full h-full flex-1 min-h-[400px]"
+            value={schema}
+            onChange={(e) => setSchema(e.target.value)}
+          />
+        </div>
+        <div className="w-full h-full flex-1 min-h-[400px]">
+          <h3 className="text-lg font-bold">Intermediate representation</h3>
+          <Textarea
+            className="w-full h-full flex-1 min-h-[400px]"
+            value={JSON.stringify(result)}
+            readOnly
+          />
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+      <div className="flex-1 min-h-[200px]">
+        <h3 className="text-lg font-bold">Components</h3>
+        <div className="grid grid-cols-3 gap-4">
+          <div>
+            {(result?.venue || result?.venue) && (
+              <Card key="venue">
+                <CardHeader>
+                  <CardTitle>Venue & Date</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>{result?.venue}</p>
+                  <p>{result?.date}</p>
+                </CardContent>
+              </Card>
+            )}
+          </div>
+          <div>
+            {timeline.map((user, i) =>
+              validIndexes.has(i) ? (
+                <Card key={user.actor}>
+                  <CardHeader>
+                    <CardTitle>{user.actor}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <p>{user.eventName}</p>
+                  </CardContent>
+                </Card>
+              ) : null
+            )}
+          </div>
         </div>
       </div>
+      <div></div>
+      <footer className="fixed bottom-0 left-0 w-full p-6 flex items-center justify-center gap-4">
+        <Button onClick={submitMessage}>
+          {loading ? (
+            <div className="flex items-center gap-2">Loading...</div>
+          ) : (
+            "start stream"
+          )}
+        </Button>
+      </footer>
+    </div>
+  );
+};
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Main;
---
+++ src/prompts/index.ts
@@ -0,0 +1,35 @@
+export const newSchema =
+  '{"venue": "string", "date": "1/2/23", "timeline": [{ "eventName": "string", "actor": "string" }]}';
+
+export const newText = `
+Paula Findlay celebrated a golden weekend on home turf, as the Canadian won the IRONMAN 70.3 Mont-Tremblant title just 48 hours after winning her third Canadian TT title on Friday.
+
+The 35-year-old, who took the time trial victory in Saint Prosper at the beginning of the weekend, drove straight to Mont-Tremblant to compete in her third race of the IRONMAN Pro Series on Sunday.
+
+Beating her fellow countrywoman Tamara Jewett and Australian Ellie Salthouse in second and third, Findlay secured her third podium of the season with a brilliant swim, bike and run in Quebec.
+
+Swim – Salthouse sets the pace
+With the 1900m swim course shortened in the wet and windy conditions to closer to 1200m, Salthouse was the first athlete out of the water in 16:57.
+
+Leading a small group, the Australian was followed out by Kristen Marchant, Gabrielle Lumkes and Findlay, with Grace Thek and Sarah True within ten seconds of the leader.
+
+Jewett, making the most of the shorter swim, was +0:37 down in tenth, with the Canadian in a good position early on to chase her first podium of the season.
+
+Bike – Findlay shows prowess on two wheels
+Having taken her third Canadian National TT title on Friday, Findlay quickly put her stamp on the race during the bike leg, with the reigning North American IRONMAN 70.3 Champion pushing the pace early on.
+
+Salthouse was able to stick close by until the 33km mark, but from there on out, it was all Findlay, as the PTO World #8 built a substantial lead over the second half of the bike to come into transition with a +2:18 lead.
+
+Salthouse, in second, looked in good form as she chased her second podium of the month after winning IRONMAN 70.3 Boulder, with Jewett and Thek down nine minutes on Findlay and just less than seven minutes back from Salthouse by the time they came into T2.
+
+
+Run – A winning weekend
+Starting the run, Findlay quickly started to put time into Salthouse and showed no signs of her time trial efforts from two days earlier, as Jewett started to run down the Australian in second.
+
+Consolidating her lead, Findlay kept building her lead back to the rest of the podium and by the second half of the run had almost five minutes over the rest of the field.
+
+Jewett, running incredibly, had reduced her deficit to Salthouse to less than a minute with four kilometres to go, with the battle for second set to go down to the wire.
+
+Up front, Findlay, who rounded out an epic weekend with a second middle distance win of the season, took the tape in 4:01:00.
+
+Behind her, Jewett managed to reel in Salthouse with less than a kilometre to go, and despite her best efforts, the Australian couldn’t match the Canadian’s sprint finish as Jewett secured a Canadian 1-2 and Salthouse closed out the podium in third.`;
